03cdd2686fd7741f1aed75e8eb6b0de44987cc2c
mstr-of-ppts-1729 <mstr-of-ppts-1729@yandex.ru>
2021-11-23 18:28:33 +0300

    Initial commit

---
+++ src/main/java/Cat.java
@@ -0,0 +1,28 @@
+import java.util.Objects;
+
+public class Cat {
+
+    private String name;
+
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cat cat = (Cat) o;
+        return Objects.equals(name, cat.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
